52632144515abbc5b73fcc54b8dfe216dea2bbe7
Karol Mlynek <karol.mlynek@microsoft.wsei.edu.pl>
2024-12-01 01:19:05 +0100

    Loading Adresy table data using WszystkieViewModel inheritance

--- MVVMFirma/ViewModels/WszystkieAdresyViewModel.cs
+++ MVVMFirma/ViewModels/WszystkieAdresyViewModel.cs
@@ -1,29 +1,35 @@
-﻿using MVVMFirma.Models.Entities;
+﻿using MVVMFirma.Helper;
+using MVVMFirma.Models.Entities;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkieAdresyViewModel : WorkspaceViewModel
-    {
-        #region Fields
-        private readonly SklepMuzycznyEntities sklepMuzycznyEntities;//to jest pole, ktore reprezentuje baze danych
-        #endregion
-        #region Properties
-
-        #endregion
+    public class WszystkieAdresyViewModel : WszystkieViewModel<Adresy>
+    {        
         #region Constructor
-
-        #endregion
-        #region Helpers
-
-        #endregion
         public WszystkieAdresyViewModel()
+            :base()
         {
             base.DisplayName = "Adresy";
         }
+        #endregion
+        #region Helpers
+        //metoda load pobiera wszystkie adresy z bazy danych
+        public override void Load()
+        {
+            List = new ObservableCollection<Adresy>
+                (
+                    sklepMuzycznyEntities.Adresy.ToList()
+                    //z bazy danych, pobieram Adres i wszystkie rekordy zamieniam na listę
+                );
+        }
+        #endregion
+
     }
 }
---
+++ MVVMFirma/ViewModels/WszystkieViewModel.cs
@@ -0,0 +1,57 @@
+﻿using MVVMFirma.Helper;
+using MVVMFirma.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMFirma.ViewModels
+{
+    public abstract class WszystkieViewModel<T> : WorkspaceViewModel//pod T beda podstawiane konkretne typy elementow listy
+    {
+        #region DB
+        protected readonly SklepMuzycznyEntities sklepMuzycznyEntities;//to jest pole, ktore reprezentuje baze danych
+        #endregion
+        #region LoadCommand
+        private BaseCommand _LoadCommand;//to jest komenda, która będzie wywoływała funkcję Load pobierającą z bazy danych
+        public ICommand LoadCommand
+        {
+            get
+            {
+                if (_LoadCommand == null)
+                    _LoadCommand = new BaseCommand(() => Load());
+                return _LoadCommand;
+            }
+        }
+        #endregion
+        #region List
+        private ObservableCollection<T> _List;//tu będą przechowywane adresy pobrane z Bazy Danych
+        public ObservableCollection<T> List
+        {
+            get
+            {
+                if (_List == null)
+                    Load();
+                return _List;
+            }
+            set
+            {
+                _List = value;
+                OnPropertyChanged(() => List);//to jest zlecenie odswiezenia listy na interfejsie
+            }
+        }
+        #endregion
+        #region Constructor
+        public WszystkieViewModel()
+        {
+            sklepMuzycznyEntities = new SklepMuzycznyEntities();
+        }
+        #endregion
+        #region Helpers
+        public abstract void Load();
+        #endregion
+    }
+}
