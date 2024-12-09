761bcbed31e70e0faa8ee9e7705d1b29d0f09825
Karol Mlynek <karol.mlynek@microsoft.wsei.edu.pl>
2024-12-09 23:32:04 +0100

    Initial view with foreign key

---
+++ MVVMFirma/Models/EntitiesForView/FakturyForAllView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.EntitiesForView
+{
+    //ta klasa jest wzorowana na klasie Faktura, tylko zamiast kluczy obcych ma czytelne dla Klienta pola (klucze obce będą zastąpione czytelnymi dla zwykłego człowieka danymi
+    public class FakturyForAllView
+    {
+        public int FakturaID { get; set; }
+        public string KlienciZamowieniaImie { get; set; }
+        public string KlienciZamowieniaNazwisko { get; set; }
+        public string KlienciZamowieniaEmail { get; set; }
+        public string KlienciZamowieniaTelefon { get; set; }
+        public DateTime? ZamowieniaDataZamowienia { get; set; }
+
+        public string ZamowieniaStatus { get; set; }
+        public decimal? ZamowieniaKwotaLaczna { get; set; }
+
+    }
+}
--- MVVMFirma/ViewModels/WszystkieFakturyViewModel.cs
+++ MVVMFirma/ViewModels/WszystkieFakturyViewModel.cs
@@ -1,16 +1,43 @@
-﻿using System;
+﻿using MVVMFirma.Models.Entities;
+using MVVMFirma.Models.EntitiesForView;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkieFakturyViewModel : WorkspaceViewModel
+    public class WszystkieFakturyViewModel : WszystkieViewModel<FakturyForAllView>
     {
+        #region Constructor
         public WszystkieFakturyViewModel()
+            : base("Faktury")
         {
-            base.DisplayName = "Faktury";
         }
+        #endregion
+        #region Helpers
+        //metoda load pobiera wszystkie adresy z bazy danych
+        public override void Load()
+        {
+            List = new ObservableCollection<FakturyForAllView>
+                (
+                   from faktura in sklepMuzycznyEntities.Faktury  //dla każdej faktury z bazy danych sklepu muzycznego
+                   select new FakturyForAllView //tworzymy nową FakturęFollAllView
+                   {
+                       FakturaID= faktura.FakturaID,
+                       KlienciZamowieniaImie= faktura.Zamowienia.Klienci.Imie,
+                       KlienciZamowieniaNazwisko = faktura.Zamowienia.Klienci.Nazwisko,
+                       KlienciZamowieniaEmail = faktura.Zamowienia.Klienci.Email,
+                       KlienciZamowieniaTelefon = faktura.Zamowienia.Klienci.Telefon,
+                       ZamowieniaDataZamowienia = faktura.Zamowienia.DataZamowienia,
+                       ZamowieniaStatus=faktura.Zamowienia.Status,
+                       ZamowieniaKwotaLaczna =faktura.Zamowienia.KwotaLaczna,
+                   }
+                );
+        }
+        #endregion
     }
 }
